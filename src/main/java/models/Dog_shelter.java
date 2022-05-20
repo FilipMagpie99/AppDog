f2ec3484d52afeb37f169341f543b9b38b6dbaff
FilipMagpie99 <sroczynski.f.99@gmail.com>
2022-05-21 00:52:27 +0200

    modele

---
+++ src/main/java/models/Dog_shelter.java
@@ -0,0 +1,62 @@
+package models;
+
+public class Dog_shelter {
+    private String name;
+    private String address;
+    private String password;
+    private Integer nip_number;
+    private Integer phone_number;
+    private String email;
+
+    public Dog_shelter(String name, String address, String password, Integer nip_number, Integer phone_number, String email) {
+        this.name = name;
+        this.address = address;
+        this.password = password;
+        this.nip_number = nip_number;
+        this.phone_number = phone_number;
+        this.email = email;
+    }
+
+    public Integer getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(Integer phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Dog_shelter() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getNip_number() {
+        return nip_number;
+    }
+
+    public void setNip_number(Integer nip_number) {
+        this.nip_number = nip_number;
+    }
+}
