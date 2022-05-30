edbb6c36d213091a5dff993ba7895c5d822ab76b
Mateusz <mateusz.kubacki7@gmail.com>
2022-05-30 14:02:22 +0200

    vaadin class

---
+++ src/main/java/com/example/demo/gui/MainPage.java
@@ -0,0 +1,8 @@
+package com.example.demo.gui;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+@Route("/ui")
+public class MainPage extends VerticalLayout {
+}
